4d1747b09244d04608f4bc0eecabcd3bd6525d92
ashish@123456 <ashishkhankari0927@gmail.com>
2023-05-09 22:40:22 +0530

    checking commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/Header/Navbar/Navbar';
+// import HotelRegistartion from './Components/Registration/HotelRegistartion';
+function App() {
+  return (
+    <div className="App">
+      {/* <HotelRegistartion/> */}
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
