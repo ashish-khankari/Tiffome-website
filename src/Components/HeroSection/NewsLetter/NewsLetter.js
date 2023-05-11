5bf05896e19b73c2da6f0b0c59dabedac4a870bc
ashish@123456 <ashishkhankari0927@gmail.com>
2023-05-11 19:05:04 +0530

    News Letter

---
+++ src/Components/HeroSection/NewsLetter/NewsLetter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NewsLetter.css'
+export const NewsLetter = () => {
+    return (
+        <div className='newsletter'>
+            <h1>Subscribe to Our NewsLetter</h1>
+            <div>
+                <p>Want to keep up to date with all our latest news and information?</p>
+                <p>Enter your email below to be added to our mailing list.</p>
+            </div>
+
+            <input placeholder='Enter Your Email' className='enter-email' />
+            <button className='sign-up'>Sign Up</button>
+        </div>
+    )
+}
