d5ad10b922869a0d5beab6268d07ee96698c92aa
ashish@123456 <ashishkhankari0927@gmail.com>
2023-05-21 22:59:58 +0530

    Seperated NavBar Login Ans SignIn Buttons

---
+++ src/Components/Home/Navbar/ShowButtons.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './ShowButtons.css'
+import { FaUser } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+export default function ShowButtons() {
+    return (
+        <div>
+            <ul className="buttons">
+                <li>
+                    <button className="log-in">
+                        <FaUser className="user-icon" />
+                        <Link to={'/logIn'}>LogIn</Link>
+                    </button>
+                </li>
+                <li>
+                    <button className="sign-in">
+                        <Link to={'/signIn'}>SignIn</Link>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+}
