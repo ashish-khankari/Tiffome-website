0f92cfe4c40a730519259b20e1d2f48e12a29684
ashish@123456 <ashishkhankari0927@gmail.com>
2023-05-21 22:00:54 +0530

    Added Authorization for Hotel Registration

--- src/Components/Home/RegisterHotel/RegisterHotel.js
+++ src/Components/Home/RegisterHotel/RegisterHotel.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styles from './RegisterHotel.module.css'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 export default function RegisterHotel() {
+
+  const navigate = useNavigate()
+
+  function handleRegisterForm(){
+
+    const loggedIn = localStorage.getItem("loggedIn")
+
+    if(loggedIn){
+      navigate('/registerHotelForm')
+    }else{
+      alert("You need to login")
+    }
+  }
   return (
     <div className={styles.hotelRegistration}>
         <h1 className={styles.text}>Register Your Services</h1>
 
-        <button className={styles.register}><Link to={'/registerHotelForm'}>Registration Form</Link></button>
+        <button className={styles.register} onClick={handleRegisterForm}>Registration Form</button>
     </div>
   )
 }
