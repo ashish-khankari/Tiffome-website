146cfe4e7cd738f4d4117c2679671ec888deaec9
ashish@123456 <ashishkhankari0927@gmail.com>
2023-05-21 22:29:33 +0530

    Added Authorization for Signed User

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,28 +1,26 @@
 import React, { useState } from 'react'
 import styles from './SignIn.module.css'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
+
 export default function SignIn() {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
+  const [list, setList] = useState([])
+
+  const navigate = useNavigate()
+
 
   function handleSubmit(e) {
     e.preventDefault()
-    submitData()
-
-  }
-
-  function submitData() {
-
     let nameRegex = /^[a-zA-Z- ]+$/g;
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/g;
     let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$/g;
 
 
-    if (password !== confirmPassword) {
-      alert("check your password")
-    }
     if (!nameRegex.test(name)) {
       alert("Enter Valid Name")
     } else
@@ -31,10 +29,7 @@ export default function SignIn() {
       } else
         if (!passwordRegex.test(password)) {
           alert("Enter Valid Password")
-        } else if (password.length !== confirmPassword.length) {
-          alert("Enter correct password")
-        } else {
-
+        } else if (password.length === confirmPassword.length || password === confirmPassword) {
           alert("ThankYou for Registration")
           const data = {
             name: name,
@@ -42,37 +37,47 @@ export default function SignIn() {
             password: password,
             confirmPassword: confirmPassword
           };
-          console.log(data);
+          // console.log(data);
           setName("")
           setEmail("")
           setPassword("")
           setConfirmPassword("")
+
+
+          setList((prevList)=>[...prevList, data])
+          localStorage.setItem("user", JSON.stringify([...list, data]))
+          navigate('/logIn')
+
+        } else {
+          alert("Check Your Password")
         }
 
+
   }
+
   return (
 
     <>
-      <form onSubmit={submitData} className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <h1>Sign-Up</h1>
         <input
           type="text"
           placeholder="Enter Your Name"
-          onChange={(e)=>setName(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
           value={name}
           className={styles.name}
         />
         <input
           type="email"
           placeholder="Enter Your Email"
-          onChange={(e)=>setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
           value={email}
           className={styles.name}
         />
         <input
           type="password"
           placeholder="Enter Your Password"
-          onChange={(e)=>setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)}
           value={password}
           className={styles.name}
 
@@ -80,15 +85,14 @@ export default function SignIn() {
         <input
           type="password"
           placeholder="Confirm Your Password"
-          onChange={(e)=>setConfirmPassword(e.target.value)}
+          onChange={(e) => setConfirmPassword(e.target.value)}
           value={confirmPassword}
           className={styles.name}
 
         />
         <p>If you are already a member, click to <span><Link to={'/logIn'} >LogIn</Link></span> </p>
-        <button onClick={handleSubmit} className={styles.btn}>Submit</button>
+        <button className={styles.btn}>Submit</button>
       </form>
-      
     </>
   )
 }
