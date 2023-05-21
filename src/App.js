b9455d93139a41b438b505cc212f1202f14bca56
ashish@123456 <ashishkhankari0927@gmail.com>
2023-05-22 01:31:09 +0530

    Added Protected Routes

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './Components/LogIn/Login';
 import SignIn from './Components/SignIn/SignIn';
 import HotelRegistartion from './Components/HotelRegistration/HotelRegistartion';
+import ProtectedRoutes from './ProtectedRoutes';
 
 function App() {
   return (
 
-   
+
     <Routes>
+      {/* protected Routes */}
+      <Route element={<ProtectedRoutes />} >
+        <Route path='/registerHotelForm' element={<HotelRegistartion />} />
+      </Route>
+
       <Route path='/' element={<Home />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/about' element={<About />} />
       <Route path='/logIn' element={<Login />} />
       <Route path='/signIn' element={<SignIn />} />
-      <Route path='/registerHotelForm' element={<HotelRegistartion/>} />
-     
+
     </Routes>
 
   );
